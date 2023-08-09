132de8b7db69478696d5684d0cfda1aacec66fa9
Tek Acharya <sarmakash430@gmail.com>
2023-08-09 10:16:14 -0400

    CPUPRODUCT-Tek.1=2: Added login page and implemented its flow. Removed index.css. For now the react-router-dom is not working as expected but I will work on it soon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-//import logo from './logo.svg';
 import React from 'react';
 import Form from './Form';
+import Login from './Login';
 import './App.css';
+import { BrowserRouter , Routes, Route} from'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <h1 className = "center-heading">Intra-National Support Foundation of America</h1>
-       <Form/>
+    <h1 >IntraNational Support Foundation of America</h1>
+    <Login/>
     </div>
   );
 }
-
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function Login(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post('http://localhost:8085/api/login', userData)
+      .then((response) => {
+        // Successful login logic (e.g., redirect to dashboard)
+        props.history.push('/form');
+      })
+      .catch((error) => {
+        setErrorMessage('Invalid email or password');
+      });
+  };
+
+  return (
+    <div className="container">
+    <div className="form-wrap">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-element">
+          <label>Email</label>
+          <input required placeholder="email/username" type="email" value={email} onChange={handleEmailChange} />
+        </div>
+        <div className="form-element">
+          <label>Password</label>
+          <input required placeholder="password" type="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button className="submit-button" type="submit">Login</button>
+        {errorMessage && <p className="error">{errorMessage}</p>}
+      </form>
+      </div>
+    </div>
+  );
+  }
+  export default Login;
